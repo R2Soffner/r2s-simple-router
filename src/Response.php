2cd813b4b623d0263bc950372b38bcda8549b0fd
PAG-Erik <erikdavinci@gmail.com>
2023-02-03 17:36:31 -0300

    update: Set Response data param default to null

--- src/Response.php
+++ src/Response.php
@@ -6,7 +6,7 @@ use R2SSimpleRouter\Enums\ResponseStatusCode;
 
 class Response
 {
-    private static function make(mixed $data = [], string $message, ResponseStatusCode $responseStatusCode, array $headers = []) {
+    private static function make(mixed $data = null, string $message, ResponseStatusCode $responseStatusCode, array $headers = []) {
         header("HTTP/1.1 {$responseStatusCode->value}");
         header('Content-Type: application/json; charset=utf-8');
         header_remove('Set-Cookie');
@@ -24,12 +24,12 @@ class Response
         exit();
     }
 
-    public static function success(mixed $data = [], string $message = 'Success', ResponseStatusCode $responseStatusCode = ResponseStatusCode::HTTP_OK)
+    public static function success(mixed $data = null, string $message = 'Success', ResponseStatusCode $responseStatusCode = ResponseStatusCode::HTTP_OK)
     {
         (new self())->make($data, $message, $responseStatusCode);
     }
 
-    public static function error(mixed $data = [], string $message = 'Error', ResponseStatusCode $responseStatusCode = ResponseStatusCode::HTTP_BAD_REQUEST)
+    public static function error(mixed $data = null, string $message = 'Error', ResponseStatusCode $responseStatusCode = ResponseStatusCode::HTTP_BAD_REQUEST)
     {
         (new self())->make($data, $message, $responseStatusCode);
     }
