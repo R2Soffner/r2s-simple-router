a60a1e20d460df27f318d17a70fd9325e7313337
PAG-Erik <erikdavinci@gmail.com>
2023-02-11 20:05:58 -0300

    feat: Add Request class

---
+++ src/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace R2SSimpleRouter;
+
+use Closure;
+
+class Request
+{
+    private Closure $routeClosure;
+
+    public function signature(): string
+    {
+        return sha1(
+            implode(
+                '|',
+                array_merge(
+                    [
+                        $_SERVER['REMOTE_ADDR'],
+                    ],
+                    $this->route()
+                )
+            )
+        );
+    }
+
+    public static function all(): array
+    {
+        return array_merge($_GET, $_POST);
+    }
+
+    public static function route(): bool|int|array|string|null
+    {
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+}
